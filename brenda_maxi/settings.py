571c8828fc572bad25ffafb517192a424b7dc722
Peter Mburu <petershowty@gmail.com>
2021-05-15 18:04:20 +0200

    Heroku Trick

--- brenda_maxi/settings.py
+++ brenda_maxi/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+STATIC_ROOT = [BASE_DIR / 'staticfiles']
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 
