445af77c6defb8dada4c877591ceacc728997e88
Peter Mburu <petershowty@gmail.com>
2021-05-15 17:43:46 +0200

    Heroku Settings

--- brenda_maxi/settings.py
+++ brenda_maxi/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_on_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,10 +88,21 @@ WSGI_APPLICATION = 'brenda_maxi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'brenda_maxi',
+        'USER': 'brenda_maxi',
+        'PASSWORD': 'brenda_maxi',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -137,3 +150,8 @@ STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 
 MEDIA_ROOT = str(BASE_DIR / 'images')
 # MEDIA_URL = '/images/'
+
+
+# Configure Django App for Heroku.
+
+django_on_heroku.settings(locals())
