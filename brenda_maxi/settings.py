638a6798996a62c78aa4b37ea49efaaa6e8b793b
Peter Mburu <petershowty@gmail.com>
2021-05-16 15:17:35 +0200

    Updated for S3 buckets

--- brenda_maxi/settings.py
+++ brenda_maxi/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
 
     # Third Party Libraries
 
-    'embed_video'
+    'embed_video',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -148,12 +149,37 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = [BASE_DIR / 'staticfiles']
+USE_S3 = 'TRUE'
+
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = 'AKIA5ZTQW37WYIGJGXWG'
+    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_STORAGE_BUCKET_NAME = 'brenda-maxi'
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = STATIC_ROOT = [BASE_DIR / 'staticfiles']
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR / 'media')
+
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = [BASE_DIR / 'staticfiles']
+#
+# STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+#
+# MEDIA_ROOT = str(BASE_DIR / 'media')
 # MEDIA_URL = '/images/'
 
 
